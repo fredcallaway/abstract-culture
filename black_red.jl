ef5057c69c444865513b5c331107e9e16a2a7e08
fredcallaway <fredc@llaway.com>
2024-02-05 16:03:34 -0500

    light refac

--- black_red.jl
+++ black_red.jl
@@ -4,16 +4,14 @@ include("r.jl")
 prob_learn(p::Float64, m::Int) = 1 - (1-p)^m
 
 @kwdef struct Environment
-    N::Int = 5
-    m::Int = 5
-    ε::Float64 = .01
-    T::Matrix{Float64} = normalize!(ones(N, N))
+    n::Int = 5  # number of starts and goals
+    m::Int = 5  # number of models
+    k::Int = 1  # number of tasks per agent
+    ε::Float64 = .01  # lapse rate
+    T::Matrix{Float64} = normalize!(ones(n, n))  # task distribution
 end
 
-function initial(env::Environment)
-    (;N) = env
-    zeros(N,N+2)
-end
+¬(p::Real) = 1 - p
 
 function prob_learn_red(b, r1, r2; ε)
     p_red =
@@ -30,12 +28,12 @@ end
 
 
 function transition(env::Environment, P::Matrix)
-    (;N, m, ε, T) = env
-    P′ = zeros(N, N+2)
-    for s in 1:N, g in 1:N
+    (;n, m, ε, T) = env
+    P′ = zeros(n, n+2)
+    for s in 1:n, g in 1:n
         b = prob_learn(P[s, g], m)
-        r1 = prob_learn(P[s, N+1], m)
-        r2 = prob_learn(P[g, N+2], m)
+        r1 = prob_learn(P[s, n+1], m)
+        r2 = prob_learn(P[g, n+2], m)
 
         p_task = T[s, g]
 
@@ -43,20 +41,22 @@ function transition(env::Environment, P::Matrix)
         p_black = 1 - p_red
 
         P′[s, g] += p_task * p_black
-        P′[s, N+1] += p_task * p_red
-        P′[g, N+2] += p_task * p_red
+        P′[s, n+1] += p_task * p_red
+        P′[g, n+2] += p_task * p_red
     end
     P′
 end
 
-function simulate(env::Environment, n_gen; P=initial(env))
-    res = [P]
+function simulate(env::Environment, n_gen; state=zeros(env.n, env.n+2))
+    res = [state]
     for i in 1:n_gen
-        P = transition(env, P)
-        push!(res, P)
+        state = transition(env, state)
+        push!(res, state)
     end
     res
 end
+
+simulate(Environment(), 4)
 
 
 # %% ==================== one task ====================
@@ -66,10 +66,11 @@ g = grid(
 
 df = dataframe(g) do prm
     env = Environment(;prm...)
+    n = env.n
     sim = simulate(env, 20)
     map(enumerate(sim)) do (gen, P)
-        black = sum(P[1:N, 1:N])
-        red = sum(P[1:N, N+1:end]) / 2
+        black = sum(P[1:n, 1:n])
+        red = sum(P[1:n, n+1:end]) / 2
         (;gen, black, red)
     end
 end
@@ -90,20 +91,20 @@ fig("red_black1")
 # %% ==================== m and n ====================
 
 env = Environment()
-N = env.N
+n = env.n
 
 g = grid(
     ε = [.05, .1, .2, .4],
     m = [1, 5, 10],
-    N = [5, 10, 20],
+    n = [5, 10, 20],
 )
 
 df = dataframe(g) do prm
     env = Environment(;prm...)
     sim = simulate(env, 20)
     map(enumerate(sim)) do (gen, P)
-        black = sum(P[1:N, 1:N])
-        red = sum(P[1:N, N+1:end]) / 2
+        black = sum(P[1:n, 1:n])
+        red = sum(P[1:n, n+1:end]) / 2
         (;gen, black, red)
     end
 end
@@ -118,8 +119,8 @@ df %>%
     teals_pal(rev=T) +
     expand_limits(y=0.4) +
     guides(color = guide_legend(reverse=TRUE)) +
-    facet_grid(m ~ N, labeller=label_glue(
-        cols='n={N}',
+    facet_grid(m ~ n, labeller=label_glue(
+        cols='n={n}',
         rows='m={m}'
     ))
 fig("red_black_grid", w=5, h=4)
