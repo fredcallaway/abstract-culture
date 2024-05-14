e109cfd3944d4f6818195fc294509516de98ebd3
fredcallaway <fredc@llaway.com>
2024-05-13 21:58:17 -0400

    generate configs for vM1g1

--- generate_redblack.jl
+++ generate_redblack.jl
@@ -4,39 +4,25 @@ using Random
 
 include("utils.jl")
 include("data.jl")
+include("graph.jl")
 
-n_start = n_goal = 4
-n_chemical = n_start + n_goal + 1
-n_mode = 8
-n_task = 5
-
-n_observed = 8
 generation = 1
+n_mode = 8
 
+Random.seed!(hash("vM1"))
 
-Random.seed!(hash("vR1.0"))
+env = red_black_env(S=4, N=10, K=5, M=20)
 
-transitions = zeros(Int, n_chemical, n_chemical)
-
-for c1 in 1:(n_start+1)
-    for c2 in (n_start+1):n_chemical
-        if c1 ≠ c2
-            transitions[c1, c2] = rand(1:n_mode)
-        end
-    end
-end
-
-all_tasks = filter(collect(Iterators.product(1:n_chemical, 1:n_chemical))[:]) do (a, b)
-    transitions[a, b] != 0
-end
-
-flat_transitions = map(all_tasks) do (a, b)
-    [a, transitions[a, b], b]
+n_state = nv(env.graph)
+T = zeros(Int, n_state, n_state)
+for t in tasks(env)
+    T[t...] = rand(1:n_mode)
 end
 
 function edges_to_recipes(edges)
-    map(edges) do (a, b)
-        [a, transitions[a, b], b]
+    map(edges) do e
+        a, b = Tuple(e)
+        [a, T[a, b], b]
     end
 end
 
@@ -45,44 +31,36 @@ zero_index(x::AbstractArray) = map(zero_index, x)
 zero_index(x::Tuple) = map(zero_index, x)
 zero_index(x::NamedTuple) = map(zero_index, x)
 
+function get_solutions(gen)
+    if gen == 0
+        gen0 = full_edges(env, simulate(env, 1)[1])
+        reduce(vcat, gen0)
+    else
+        participants = load_participants("vM1g$gen")
+        @rsubset! participants :generation == gen - 1
+        uids = participants.uid
+        trials = mapreduce(load_trials, vcat, uids)
 
-if generation == 1
-    foreach(1:30) do i
-        recipes = unique(sample(flat_transitions, n_observed))
-
-        write("../machine-task/static/json/middle-$i.json", json((;
-            generation,
-            transitions = zero_index(transpose(transitions)),
-            tasks = zero_index(sample(all_tasks, n_task, replace=false)),
-            recipes = zero_index(recipes),
-            nChemical = n_chemical,
-            nMode = n_mode,
-        )))
-    end
-else
-    participants = load_participants("v4.0")
-    @rsubset! participants :generation == generation - 1
-    uids = participants.uid
-    trials = mapreduce(load_trials, vcat, uids)
-
-    solutions = map(trials) do t
-        Tuple.(t.path)
-    end
-
-    Random.seed!(hash("vR1.0$generation"))
-    foreach(1:30) do i
-        recipes = edges_to_recipes(unique(reduce(vcat, sample(solutions, 10, replace=false))))
-        @assert !isnothing(example)
-
-        write("../machine-task/static/json/middle-$i.json", json((;
-            generation,
-            transitions = zero_index(transpose(transitions)),
-            tasks = zero_index(sample(all_tasks, n_task, replace=false)),
-            recipes = zero_index(recipes),
-            nChemical = n_chemical,
-            nMode = n_mode,
-        )))
+        map(trials) do t
+            t.path
+        end
     end
 end
 
+function sample_recipes(gen)
+    observed_solutions = sample(get_solutions(gen-1), env.M; replace=true)
+    obs_edges = unique(reduce(vcat, observed_solutions))
+    edges_to_recipes(obs_edges)
+end
+
+foreach(1:30) do i
+    write("../machine-task/static/json/middle-$i.json", json((;
+        generation,
+        transitions = zero_index(transpose(T)),
+        tasks = zero_index(sample(tasks(env), env.K, replace=true)),
+        recipes = zero_index(sample_recipes(generation)),
+        nChemical = nv(env.graph),
+        nMode = n_mode,
+    )))
+end
 
