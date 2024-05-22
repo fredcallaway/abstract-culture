0f6a23fdaa0734cbaef051830a324c1f7b46c57c
fredcallaway <fredc@llaway.com>
2024-05-22 10:30:47 -0700

    power analysis on N

--- select_env.jl
+++ select_env.jl
@@ -50,41 +50,63 @@ fig(w=10, h=7)
 
 # %% --------
 
+R"""
+df %>%
+    fctrize(M, levels=c(20, 5, 50)) %>%
+    regress(compositionality ~ M, print_table=FALSE) %>%
+    tidy %>%
+    filter(term != "(Intercept)") %>%
+    with(max(p.value))
+
+"""
+
 
 R"""
-groups = fixations %>%
-    filter(presentation != n_pres) %>%
-    mutate(
-        fixated = case_when(
-            mod(presentation, 2) == 1 ~ pretest_accuracy_first,
-            mod(presentation, 2) == 0 ~ pretest_accuracy_second,
-        ),
-        nonfixated = case_when(
-            mod(presentation, 2) == 1 ~ pretest_accuracy_second,
-            mod(presentation, 2) == 0 ~ pretest_accuracy_first,
-        )
-    ) %>%
-    mutate(relative = fixated - nonfixated) %>%
-    select(wid, duration, fixated) %>%
-    nest_by(wid, .keep=TRUE) %>%
-    with(data)
 
-sample_p = function(N, run_model) {
-    data = sample(groups, N, replace=T) %>%
-        map(~ mutate(.x, wid=round(1e10 * runif(1)))) %>%
-        bind_rows
+
+sample_p = function(groups, n_chain, run_model) {
+    data = sample(groups, n_chain, replace=T) %>% bind_rows
     tryCatch(run_model(data), error=function(c) NaN)
 }
 
-power_analysis = function(N, n_sim, run_model) {
-    results = map(N, ~ replicate(n_sim, sample_p(.x, run_model))) %>% unlist
+power_analysis = function(N, n_chain, n_sim, run_model) {
+    groups = df %>%
+        filter(N == {{N}}) %>%
+        fctrize(M, levels=c(20, 5, 50)) %>%
+        nest_by(population, .keep=TRUE) %>%
+        with(data)
+    # results = map(n_chain, ~ replicate(n_sim, sample_p(groups, .x, run_model))) %>% unlist
     expand.grid(
         sim_i = 1:n_sim,
-        N = N
+        n_chain = n_chain
     ) %>%
     rowwise() %>%
     mutate(
-        p = sample_p(N, run_model)
+        p = sample_p(groups, n_chain, run_model)
     ) %>% ungroup()
 }
+
+n_sim = 300
+n_chain = c(1, 2, 4, 8)
+
+pwr = map(c(10, 12,14, 16, 18, 20), ~
+    power_analysis(.x, n_chain, n_sim, . %>%
+        regress(compositionality ~ M, print_table=FALSE) %>%
+        tidy %>%
+        filter(term != "(Intercept)") %>%
+        with(max(p.value))
+    ) %>%
+    mutate(N = .x) %>%
+    group_by(N, n_chain) %>%
+    summarise(power = mean(p < .05))
+) %>% bind_rows
+
+"""
+
+R"""
+pwr %>%
+    ggplot(aes(n_chain, power, color=factor(N))) +
+    geom_line(linewidth=.5) + teals_pal()
+
+fig()
 """
