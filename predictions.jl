9f79626d7c6a5157e677f966548da5dbf19d5666
fredcallaway <fredc@llaway.com>
2025-05-14 22:04:58 -0400

    pull rational policy from predictions (unused)

--- predictions.jl
+++ predictions.jl
@@ -3,7 +3,6 @@ using DataFrames, CSV
 using JSON
 using Optim
 
-
 # %% --------
 
 version = "v23"
@@ -24,7 +23,6 @@ function parse_policy(file)
 end
 
 human_policy = parse_policy("tmp/compositional-rates-code-pilot-$version.csv")
-baseline = agent_policy.table[1, 1]
 
 N = [32, 50, 100]
 D = [2, 8, 32]
@@ -53,7 +51,6 @@ function epsilon_policy(ε=0.5)
     TabularPolicy(@. X * (1 - ε) + (1 - X) * ε)
 end
 
-
 using Optim
 res = optimize(0, 1) do ε
     abs.(epsilon_policy(ε).table .- human_policy.table) |> sum
@@ -61,7 +58,6 @@ end
 res.minimizer
 
 make_predictions(epsilon_policy(res.minimizer)) |> CSV.write("tmp/predictions-epsilon-$version.csv")
-
 
 # %% --------
 
@@ -74,58 +70,3 @@ flatmap([2, 8, 32]) do D
         (;D, task = string(x.a, x.b), kind = x.red ? "bespoke" : "compositional")
     end
 end |> json |> write("../machine-task/stimuli/simulated_solutions.json")
-
-
-# %% --------
-
-imap(red_rate.(simulate(env, 10))[2:end]) do gen, compositionality
-    (;gen, compositionality)
-end
-
-
-logistic(x) = 1 / (1 + exp(-x))
-lapse(p, ε) = (1 - ε) * p + ε / 2
-
-function cost_table(;action_cost=1, search_cost=4, n_bespoke=4, n_compositional=6)
-    map(product(0:1, 0:3, false:true)) do (b, c, do_comp)
-        if do_comp
-            c_search = 1 - (0.5min(c, 2))
-            n_compositional * (action_cost + search_cost * c_search)
-            # compositional_cost + 1.5
-        else
-            b_search = 1. - b
-            bespoke_cost = n_bespoke * (action_cost + search_cost * b_search)
-        end
-    end
-end
-
-C = cost_table()
-
-
-
-
-# %% --------
-
-function get_rational_policy(β, ε)
-    map(diff(cost_table(), dims=3)[:, :]) do bespoke_advantage
-        p = logistic(β * -bespoke_advantage)
-        lapse(p, ε)
-        # bespoke_cost - compositional_cost
-        # bespoke_cost, compositional_cost
-    end |> TabularPolicy
-end
-
-softmax_policy = get_rational_policy(0.3, 0.05)
-softmax_policy.table
-
-using Optim
-res = optimize(0, 1) do β
-    abs.(get_rational_policy(β, .05).table .- human_policy.table) |> sum
-end
-res.minimizer
-get_rational_policy(res.minimizer, 0.05).table
-human_policy.table
-
-cost_table()
-
-diff(cost_table(), dims=3)[:, :]
