aff87c476be0e3f3c90a88b2f802fac861b2517f
fredcallaway <fredc@llaway.com>
2025-06-06 19:23:16 -0400

    WIP fullpop (scratch)

---
+++ scratch.jl
@@ -0,0 +1,57 @@
+include("infinite_env.jl")
+include("utils.jl")
+
+# %% --------
+
+env = InfiniteEnv(p_0 = 0.0, p_r = 1.0)
+
+function analytic_transition(env, c)
+    (;S, D) = env
+    b = 1 - c
+    no_bespoke = (1 - b/S^2)^D
+    p = (c * (2S - 1/S^2) / S^2) / (1 - b/S^2)
+    any_comp = prob_observe(p, D)
+    no_bespoke * any_comp
+end
+
+c = .5
+transition(env, CompPop(c))
+analytic_transition(env, c)
+nothing
+
+struct FullPop{S}
+    C::SMatrix{S,S,Float64}
+    B::SMatrix{S,S,Float64}
+end
+
+# %% --------
+
+function transition(env::InfiniteEnv, pop::FullPop)
+    (;S, D, p_0, p_r) = env
+
+    (;B, C) = pop
+    
+    B1 = similar(pop.B); C1 = similar(pop.C)
+    for i in 1:S, j in 1:S
+        term1 = 1/S^2
+        term2 = (1 - B[i,j])^D
+        term3_inner = (sum(C[i, :]) + sum(C[:, j]) - C[i,j]) / (1 - B[i,j])
+        term3 = 1 - (1 - term3_inner)^D
+        C1[i,j] = term1 * term2 * term3
+        B1[i, j] = term1 - C1[i,j]
+    end
+    FullPop{S}(C1, B1)
+end
+
+for c in 0:.1:1
+    S = env.S
+    pop = FullPop{S}(
+        (c/S^2) * ones(S, S), 
+        ((1-c)/S^2) * ones(S, S)
+    )
+    @assert sum(pop.C) + sum(pop.B) ≈ 1
+    pop1 = transition(env, pop)
+    @assert sum(pop1.C) + sum(pop1.B) ≈ 1
+
+    @assert sum(pop1.C) ≈ transition(env, CompPop(c)).comp
+end
