a833c035eff217f7a408952c1545654970699812
fredcallaway <fredc@llaway.com>
2025-06-23 11:00:08 -0400

    plot single evolution curve

--- plot_evolution.jl
+++ plot_evolution.jl
@@ -1,13 +1,32 @@
 include("utils.jl")
 include("infinite_env.jl")
 
-
 using RCall
 R_DIR = joinpath(@__DIR__, "r")
 R"""
 setwd($R_DIR)
 source("base.r")
 FIGS_PATH <- "figs/model/"
+"""
+
+# %% --------
+
+env = InfiniteEnv(;S=10, D=50, p_r=1.)
+
+df = map(enumerate(simulate(env, 30; init=1e-8))) do (gen, pop)
+    (;gen=gen-1, compositionality=compositional_rate(pop))
+end |> DataFrame
+
+@rput df
+
+R"""
+figure("evolution_single", df %>%
+    ggplot(aes(gen, compositionality)) +
+    geom_line(color=RED) + expand_limits(y=c(0, 1)) +
+    no_legend +
+    no_gridlines +
+    theme()
+)
 """
 
 # %% --------
